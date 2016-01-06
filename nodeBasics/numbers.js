3b07853d09503a3ec7c3441032f7515d5b4e4dbe
Randy Davis <randy_davis@outlook.com>
2016-01-06 00:04:53 -0600

    finish creating numbers example

---
+++ nodeBasics/numbers.js
@@ -0,0 +1,34 @@
+//var age = 35;
+//var results;
+//
+//results = age + 6;
+//console.log('age + 6 = ' + results);
+//
+//results = age - 1;
+//console.log('age - 1 = ' + results);
+//
+//
+//results = age * 3;
+//console.log('age * 3 = ' + results);
+//
+//results = age / 6;
+//console.log('age / 6 = ' + results);
+//
+//results = age % 5;
+//console.log('age % 5 = ' + results);
+//
+//results = results - 1;
+//console.log(results--);
+//
+//var num = 19;
+//console.log(num++);
+//console.log(num);
+
+
+var grade1 = 92,
+    grade2 = 98,
+    grade3 = 83,
+    avgGrade = 0;
+
+avgGrade = (grade1 + grade2 + grade3)/3;
+console.log('Your average grade is ' + avgGrade);
