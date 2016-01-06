54f2ebb8efb755c671d3222df6ac2899cee26618
Randy Davis <randy_davis@outlook.com>
2016-01-05 23:49:58 -0600

    finish creating strings example

---
+++ nodeBasics/strings.js
@@ -0,0 +1,10 @@
+//var name = 'Randy';
+//var lastName = 'Davis';
+//console.log(name + " " + lastName);
+//console.log(lastName.length);
+
+var firstName = 'Randy',
+    lastName = 'Davis',
+    fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(fullName.length);
