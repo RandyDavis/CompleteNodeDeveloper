a23aa362ffedf42e3237addaa74f15c463f9d212
Randy Davis <randy_davis@outlook.com>
2016-01-06 20:32:26 -0600

    finish if statements example

---
+++ nodeBasics/if.js
@@ -0,0 +1,22 @@
+//var averageGrade = 100,
+//    name = 'Jimmy';
+//
+//if (averageGrade === 100 && name === 'Randy') {
+//    console.log('Congratulations! You got a perfect grade!');
+//} else if (averageGrade >= 70) {
+//    console.log('Congratulations! You paassed the class!');
+//} else {
+//    console.log('Sorry, but you failed the class');
+//}
+
+
+var friend = 'Fred';
+
+
+if (friend === 'Jane' || friend === 'Bill' || friend === 'Amanda') {
+    console.log('Hello friend!');
+} else if (friend === 'Fred' || friend === 'Chuck') {
+    console.log('Hey...');
+} else {
+    console.log('Hello stranger..');
+}
