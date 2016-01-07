49d5ffe708b6dd9ab6f068c67a31e878a1ac25de
Randy Davis <randy_davis@outlook.com>
2016-01-06 21:02:32 -0600

    finish functions example

---
+++ nodeBasics/functions.js
@@ -0,0 +1,16 @@
+//function greetUser (name, lastName) {
+//    return 'Hello ' + name + ' ' + lastName + '!'
+//}
+//
+//var greeting = greetUser('Randy', 'Davis');
+//
+//console.log(greeting);
+//greetUser('Jen', 'Smith');
+
+function add (a, b) {
+    return a + b;
+}
+
+var result = add(3, 5);
+
+console.log(result);
